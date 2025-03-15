7f85306c9e35a53096f891af9875f87ab10bcdf0
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-03-16 01:34:45 +0545

    add signup, login and logout APIs

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,10 +16,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
-    debugger
-    super do |resource|
-      debugger
-    end
+    super
   end
 
   # DELETE /resource/sign_out
@@ -32,5 +29,56 @@ class Users::SessionsController < Devise::SessionsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [ :phone, :email, :password ])
+  end
+
+  private
+
+  def respond_with(resource, _opt = {})
+    @token = request.env["warden-jwt_auth.token"]
+    return head :unauthorized unless @token
+
+    headers["Authorization"] = @token
+
+    render json: {
+      status: 200,
+      message: "Logged in successfully.",
+      token: @token,
+      data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+    }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    return head :unauthorized unless request.headers["Authorization"].present?
+
+    begin
+      jwt_payload = JWT.decode(
+        request.headers["Authorization"].split.last,
+        Rails.application.credentials.devise_jwt_secret_key!
+      ).first
+
+      current_user = User.find_by(id: jwt_payload["sub"])
+
+      if current_user
+        if current_user.jti != jwt_payload["jti"]
+          return render json: { status: 401, message: "User already logged out." }, status: :unauthorized
+        end
+
+        current_user.update!(jti: SecureRandom.uuid)
+        render json: {
+          status: 200,
+          message: "Logged out successfully."
+        }, status: :ok
+      else
+        render json: {
+          status: 401,
+          message: "Invalid session"
+        }, status: :unauthorized
+      end
+    rescue JWT::DecodeError, JWT::VerificationError
+      render json: {
+        status: 401,
+        message: "Invalid token"
+      }, status: :unauthorized
+    end
   end
 end
