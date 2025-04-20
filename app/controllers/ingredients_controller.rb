161edd7ae169bbd751d6eeacfa767dd1e6668239
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-04-20 19:11:33 +0545

    refactor fetch_ingredients api

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -4,7 +4,7 @@ class IngredientsController < ApplicationController
   def index
     ingredient_contents = []
     ingredients = Ingredient.limit(params[:limit]).offset(params[:offset]).map do |ingredient|
-      ingredient_contents << ingredient.name
+      ingredient_contents << ingredient
     end
     render json: ingredient_contents
   end
