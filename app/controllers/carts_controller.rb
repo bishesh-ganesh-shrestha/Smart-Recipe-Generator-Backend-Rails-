189599b29fdfdb12575e257f6a831336291d1c79
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-04-26 17:01:40 +0545

    add authentication in cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,6 @@
 class CartsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     render json: { status: 200, cart: { ingredients: current_user.cart.ingredients } }
   end
